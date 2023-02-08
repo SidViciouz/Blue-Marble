558a69aa1b2e7455036e1ba9c0eda55a4a930d8e
DESKTOP-8O9QGD1\82102 <han53461@gmail.com>
2023-02-08 10:45:29 +0900

    fence 객체 생성

--- Rocket/Pipeline.cpp
+++ Rocket/Pipeline.cpp
@@ -5,4 +5,8 @@ void Pipeline::Initialize()
 	IfError::Throw(D3D12CreateDevice(nullptr, D3D_FEATURE_LEVEL_11_1, IID_PPV_ARGS(&mDevice)),
 		L"create device error!");
 
+	IfError::Throw(mDevice->CreateFence(0, D3D12_FENCE_FLAG_NONE,IID_PPV_ARGS(mFence.GetAddressOf())),
+		L"create fence error!");
+
+
 }
--- Rocket/Pipeline.h
+++ Rocket/Pipeline.h
@@ -9,6 +9,12 @@ class Pipeline
 {
 private:
 	ComPtr<ID3D12Device> mDevice = nullptr;
+	ComPtr<ID3D12Fence> mFence = nullptr;
+
+	UINT64 mFenceValue = 0;
+	DXGI_FORMAT mBackBufferFormat = DXGI_FORMAT_R8G8B8A8_UNORM;
+	DXGI_FORMAT mDepthStencilBufferFormat = DXGI_FORMAT_D24_UNORM_S8_UINT;
+
 
 public:
 	void Initialize();
