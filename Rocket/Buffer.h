e550950b4b68b8ad4d58ca7215ea0553873d268a
SidViciouz <70373435+SidViciouz@users.noreply.github.com>
2023-02-10 13:26:53 +0900

    Buffer class에서  Buffer Interface class를 분리시켜서 Buffer는 이를 상속하도록함. UploadBuffer 도 Buffer Interface를 상속하도록 구현 (UploadBuffer은 intermediate이 없는 경우)

--- Rocket/Buffer.h
+++ Rocket/Buffer.h
@@ -4,7 +4,38 @@
 
 using namespace Microsoft::WRL;
 
-class Buffer
+class BufferInterface
+{
+public:
+	BufferInterface(const BufferInterface& buffer) = delete;
+	BufferInterface(BufferInterface&& buffer) = delete;
+	BufferInterface& operator=(const BufferInterface& buffer) = delete;
+	BufferInterface& operator=(BufferInterface&& buffer) = delete;
+
+	virtual void Copy(const void* data, int byteSize, ID3D12GraphicsCommandList* commandList) = 0;
+
+protected:
+	BufferInterface(ID3D12Device* device, int byteSize);
+
+	ComPtr<ID3D12Resource> mUploadBuffer;
+};
+
+class UploadBuffer : public BufferInterface
+{
+public:
+	UploadBuffer(ID3D12Device* device, int byteSize);
+	UploadBuffer(const UploadBuffer& buffer) = delete;
+	UploadBuffer(UploadBuffer&& buffer) = delete;
+	UploadBuffer& operator=(const UploadBuffer& buffer) = delete;
+	UploadBuffer& operator=(UploadBuffer&& buffer) = delete;
+
+	virtual void Copy(const void* data, int byteSize,ID3D12GraphicsCommandList* commandList) override;
+
+protected:
+
+};
+
+class Buffer : public BufferInterface
 {
 public:
 	Buffer(ID3D12Device* device, int byteSize);
@@ -13,9 +44,8 @@ public:
 	Buffer& operator=(const Buffer& buffer) = delete;
 	Buffer& operator=(Buffer&& buffer) = delete;
 
-	void Copy(const void* data, int byteSize,ID3D12GraphicsCommandList* commandList);
+	virtual void Copy(const void* data, int byteSize, ID3D12GraphicsCommandList* commandList) override;
 
-private:
+protected:
 	ComPtr<ID3D12Resource> mBuffer;
-	ComPtr<ID3D12Resource> mUploadBuffer;
 };
