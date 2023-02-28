19691e57a08b13c2f584e77e6a5d19d27aee9f62
SidViciouz <70373435+SidViciouz@users.noreply.github.com>
2023-02-28 16:47:16 +0900

    sphere volume에 빛 적용
    sphere volume에 빛 적용

--- Rocket/VolumeShader.hlsl
+++ Rocket/VolumeShader.hlsl
@@ -53,6 +53,7 @@ static Coord coord[6] = {
 	{-1.0f,-1.0f}
 };
 
+//rayDir는 normalize된 상태여야함.
 bool SphereIntersect(in float3 rayOrigin,in float3 rayDir,out float tMin,out float tMax)
 {
 	float3 center = float3(0.0f, 0.0f, 0.0f);
@@ -80,6 +81,33 @@ bool SphereIntersect(in float3 rayOrigin,in float3 rayDir,out float tMin,out flo
 	return true;
 }
 
+float3 sphereLi(float3 position,Light light)
+{
+	float tMin;
+	float tMax;
+	float lightToPos = position - light.position;
+	//point light로 가정
+	SphereIntersect(light.position, normalize(lightToPos), tMin, tMax);
+
+	float stepSize = 0.1f;
+	int steps = (length(lightToPos) - tMin)/stepSize;
+	float sigmaT = 0.2f;
+	float result = 0.0f;
+	float att = 1.0f;
+
+	//result = exp((tMin - length(lightToPos)) * sigmaT);
+
+	
+	for (int i = 0; i < steps; ++i)
+	{
+		att *= exp(-stepSize * sigmaT);
+	}
+	result += att;
+	
+
+	return light.color * result;
+}
+
 VertexOut VS( uint vertexId : SV_VertexID )
 {
 	VertexOut vout;
@@ -96,19 +124,27 @@ float4 PS(VertexOut pin) : SV_Target
 	float tMin;
 	float tMax;
 	float3 dir = normalize(pin.posW.xyz - cameraPosition.xyz);
+	float3 origin = cameraPosition.xyz;
 
-	if (!SphereIntersect(cameraPosition.xyz, dir, tMin, tMax))
+	if (!SphereIntersect(origin, dir, tMin, tMax))
 		clip(-1);
 
 	float stepSize = 0.1f;
 	int steps = (tMax - tMin) / stepSize;
+	float sigmaT = 0.2f;
+	float att = 1.0f;
+	float3 result = 0.0f;
 
-	
 	for (int i = 0; i < steps; ++i)
 	{
-		pin.color.x += 0.01f;
-		pin.color.w -= 0.01f;
+		float3 ray = origin + (i*stepSize + tMin)*dir;
+		att *= exp(-stepSize * sigmaT);
+		result += sphereLi(ray,lights[0]) * att * stepSize;
+		
+		//pin.color.w -= 0.01f;
 	}
+	pin.color.w -= result.x;
+	pin.color.xyz += result.xyz;
 
 	return pin.color;
 }
