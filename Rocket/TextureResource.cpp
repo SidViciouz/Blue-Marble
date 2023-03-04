12354919777f0de33aabb54f0258e36722fbad84
SidViciouz <70373435+SidViciouz@users.noreply.github.com>
2023-03-04 23:43:20 +0900

    TextureResource class 추가

---
+++ Rocket/TextureResource.cpp
@@ -0,0 +1,6 @@
+#include "TextureResource.h"
+
+void TextureResource::Copy(void* pData, int width, int height)
+{
+
+}
---
+++ Rocket/TextureResource.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Util.h"
+
+//일단 2d텍스처를 다룬다.
+class TextureResource
+{
+public:
+	TextureResource(const TextureResource& t) = delete;
+	TextureResource(TextureResource&& t) = delete;
+	TextureResource& operator=(const TextureResource& t) = delete;
+	TextureResource& operator=(TextureResource&& t) = delete;
+
+	void Copy(void* pData,int width, int height);
+
+private:
+	ComPtr<ID3D12Resource> mUploadBuffer;
+	ComPtr<ID3D12Resource> mTextureResource;
+
+};
